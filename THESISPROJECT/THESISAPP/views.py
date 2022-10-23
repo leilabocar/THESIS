bdea7a6d24c4a3d414f3c888359d0ed5f3495190
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-10-23 23:32:10 +0800

    .

--- THESISPROJECT/THESISAPP/views.py
+++ THESISPROJECT/THESISAPP/views.py
@@ -1,7 +1,8 @@
 from http.client import HTTPResponse
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from django.http.response import HttpResponse
 
 def index(request):
-    return HTTPResponse('Homepage')
+    return HttpResponse('Homepage')
 
