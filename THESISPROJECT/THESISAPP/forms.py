625c5b1f036f081360685ad8f6770b08ef440aad
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-10-24 15:33:32 +0800

    .

---
+++ THESISPROJECT/THESISAPP/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
+
+class SignupForm(UserCreationForm):
+    first_name = forms.CharField()
+    last_name = forms.CharField
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','username','email','password1','password2','is_admin','is_client')
