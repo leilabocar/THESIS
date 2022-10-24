2c5891e35aaf288092713c2f001c8e2deb3536d8
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-10-24 20:22:06 +0800

    .

--- THESISPROJECT/THESISAPP/forms.py
+++ THESISPROJECT/THESISAPP/forms.py
@@ -3,16 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import *
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField()
+    username = forms.CharField(label="username")
+    password = forms.CharField(label="password2")
 
 class SignupForm(UserCreationForm):
     first_name = forms.CharField()
     last_name = forms.CharField
-    username = forms.CharField()
-    email = forms.CharField()
+    username = forms.CharField(label="username")
+    email = forms.CharField(label="email")
     password1 = forms.CharField()
-    password2 = forms.CharField()
+    password2 = forms.CharField(label="password2")
 
     class Meta:
         model = User
