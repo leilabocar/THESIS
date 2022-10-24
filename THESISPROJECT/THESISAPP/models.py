22b447cc7abd74a03dc3e602ecda266ccb74f209
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-10-24 15:05:51 +0800

    .

--- THESISPROJECT/THESISAPP/models.py
+++ THESISPROJECT/THESISAPP/models.py
@@ -1,3 +1,8 @@
+from email.policy import default
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    is_admin = models.BooleanField('Is admin', default=False)
+    is_client = models.BooleanField('Is client', default=False)    
--- THESISPROJECT/THESISPROJECT/settings.py
+++ THESISPROJECT/THESISPROJECT/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'THESISPROJECT.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'thesistest',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -129,3 +133,5 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+AUTH_USER_MODEL = 'THESISAPP.User'
