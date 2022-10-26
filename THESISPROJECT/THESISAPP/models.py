15c7fe0711cfb8e6a186bc3791aaaec9e233bdd7
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-10-26 22:24:46 +0800

    Update models.py

--- THESISPROJECT/THESISAPP/models.py
+++ THESISPROJECT/THESISAPP/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     is_admin = models.BooleanField('Is admin', default=False)
-    is_client = models.BooleanField('Is client', default=False)    
+    is_client = models.BooleanField('Is client', default=False)   
