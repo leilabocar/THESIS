79e2bf33f73731078c3f1bb70830ed7f521f4f33
androuhh <markandrew.ramos@gsfe.tupcavite.edu.ph>
2022-12-08 14:32:22 +0800

    .

--- THESISPROJECT/THESISAPP/filters.py
+++ THESISPROJECT/THESISAPP/filters.py
@@ -1,6 +1,8 @@
 import django_filters
-
+from django import forms
 from .models import *
+from .widget import DatePickerInput, TimePickerInput, DateTimePickerInput
+from django.db.models import ForeignKey
 
 class buyersFilter(django_filters.FilterSet):
     lot_choices=[('Lawn Lot','Lawn Lot'),
@@ -21,6 +23,10 @@ class buyersFilter(django_filters.FilterSet):
         fields = '__all__'
 
 class appointmentFilter(django_filters.FilterSet):
+    date = django_filters.DateFilter(widget=DatePickerInput(attrs={'class':'form-control'}))
+    fullname = django_filters.CharFilter(lookup_expr='icontains')
+    reason = django_filters.CharFilter(lookup_expr='icontains')
+
     class Meta:
         model = BookAppointmentModel
         fields = '__all__'
@@ -32,8 +38,21 @@ class clientpaymentFilter(django_filters.FilterSet):
                    ('3 Years','3 Years'),
                    ('Full Down','Full Down'),
                    ('Reservation','Reservation')]
+    
+    lot_choices=[('Lawn Lot','Lawn Lot'),
+                      ('Mausoleum','Mausoleum'),
+                      ('Niche','Niche'),
+                      ('Apartment','Apartment')]
 
     terms = django_filters.ChoiceFilter(empty_label='Terms',choices=terms_choices)
+    customer = django_filters.CharFilter(field_name='customer__username',lookup_expr='icontains')
+    product_lottype = django_filters.ChoiceFilter(empty_label='Lot Type:',choices=lot_choices,field_name='product__lot')
+    product_phase = django_filters.CharFilter(field_name='product__phase')
+    product_block = django_filters.CharFilter(field_name='product__block')
+    product_lotno = django_filters.CharFilter(field_name='product__lotno')
+    due_date = django_filters.DateFilter(widget=DatePickerInput(attrs={'class':'form-control','onfocus':'(this.type=date)'}))
+    paid_date = django_filters.DateFilter(widget=DatePickerInput(attrs={'class':'form-control'}))
+
     class Meta:
         model = LotOrder
         fields = '__all__'
@@ -45,6 +64,9 @@ class inquiryFilter(django_filters.FilterSet):
                       ('Apartment','Apartment')]
 
     lot_type = django_filters.ChoiceFilter(empty_label='Select Lot Type',choices=lot_choices)
+    phase = django_filters.CharFilter(lookup_expr='icontains')
+    block = django_filters.CharFilter(lookup_expr='icontains')
+    lotno = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = InquiryFormModel
         fields = '__all__'
@@ -56,6 +78,10 @@ class productFilter(django_filters.FilterSet):
                       ('Apartment','Apartment')]
 
     lot = django_filters.ChoiceFilter(empty_label='Select Lot Type',choices=lot_choices)
+    deceased = django_filters.CharFilter(lookup_expr='icontains')
+    phase = django_filters.CharFilter(lookup_expr='icontains')
+    block = django_filters.CharFilter(lookup_expr='icontains')
+    lotno = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Product
         fields = '__all__'
